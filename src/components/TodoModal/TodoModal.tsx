d89a551ebcf1c645b293110402c221faf6390b42
Uandrik Laptop <uandriy4141@gmail.com>
2025-11-10 00:53:11 +1400

    Solution

--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -62,7 +62,7 @@ export const TodoModal: React.FC<Props> = ({
 
               {' by '}
 
-              <a href={user.email}>{user.name}</a>
+              <a href={`mailto:${user.email}`}>{user.name}</a>
             </p>
           </div>
         </div>
